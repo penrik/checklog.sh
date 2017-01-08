4e84d1a1f48f4857b5f4c28d6cd5411ff004ce50
Henrik Hartelius <henrik@hartelius.org>
2017-01-08 19:30:20 +0000

    Initial commit

---
+++ logcheck.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+LOGFILE=$1
+WORD=$2
+
+OLDSIZE=$(cat "bytes.db")
+
+NEWSIZE=$(stat -c%s "$LOGFILE")
+echo "$NEWSIZE" > bytes.db
+
+if [ $NEWSIZE \> $OLDSIZE ];
+then 
+	COUNT=`expr $NEWSIZE - $OLDSIZE`
+else
+	COUNT=0
+fi;
+
+if tail -c "$COUNT" "$LOGFILE" | grep --quiet "$WORD"; then
+	echo CRITICAL
+	exit 2
+else
+	echo OK
+	exit 0
+fi
